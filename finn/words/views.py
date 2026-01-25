c7d603b5e28f7d174f2b61f27d35a8a91816518e
t3ugeg00 <t3ugeg00@students.oamk.fi>
2026-01-25 20:28:30 +0200

    Allow users to add words

--- finn/words/views.py
+++ finn/words/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import WordForm
 
 # Create your views here.
 
 def user_words(req):
     if req.user.is_authenticated:
+        if req.method == "POST":
+            form = WordForm(req.POST)
+            if form.is_valid():
+                word = form.save(commit=False)
+                word.user = req.user
+                word.save()
+                return redirect('home')
+        else:
+            form = WordForm()
         words = req.user.word.all()
-        return render(req, 'home.html', {'words': words})
+        return render(req, 'home.html', {'words': words, 'form': form})
     else:
         return render(req, 'home.html', {'words': []})
